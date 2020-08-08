5ce7b0f406637ca7fcfbfc2b01b594caf4b19d3d
薛宁 <xnsleet@163.com>
2020-08-08 14:01:25 +0800

    第一次提交

---
+++ src/main/java/com/xn/sleet/SleetApplication.java
@@ -0,0 +1,13 @@
+package com.xn.sleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xn/sleet/SleetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xn.sleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
